d68b867880b2024ee4ed0b5e85571a6b58a06157
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-08-01 05:21:18 +0000

    done

---
+++ reverse_num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class reverse_num {
+  public static void main(String args[]) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the number to be reversed:");
+    int a =sc.nextInt();
+    int rev_num = 0;
+    while(a != 0) {
+      int rem = a % 10;
+      rev_num = rev_num * 10 + rem;
+      a = a / 10;
+    }
+    System.out.println("Revesed number is :"+ rev_num);
+  }
+}
